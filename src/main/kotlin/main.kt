c96a8e70168a03f45db97e5d2081d21e3b773e20
Rainer Ramos <rainermr89@hotmail.com>
2021-07-26 15:52:38 -0300

    Herança

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
+//a palavra OPEN permite q a classe seja herdada
+
+open class Maquina (val marca: String) {
+    fun minhaMarca(){
+        println("Minha marca é $marca")
+    }
+}
+
+class Computador(marca: String, val nucleos: Int) : Maquina(marca) {
+        fun ligar() {}
+        fun processar () {}
+}
+
 fun main (){
-    
+    val c: Computador = Computador("xpto", 10)
+    with(c) {
+        ligar()
+        processar()
+        minhaMarca()
+    }
 }
