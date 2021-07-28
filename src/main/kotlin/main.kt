740bc27d17cb70e5b53ad81972b64bcae75205a0
Rainer Ramos <rainermr89@hotmail.com>
2021-07-28 19:04:53 -0300

    Polimorfismo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,40 @@
-class Pessoa {
-   lateinit var nome: String
+interface Funcionario {
+    fun calculaBonus()
+}
 
-    fun geradorDeNome(){
-        nome = "haiddag"
+class Gerente : Funcionario {
+    override fun calculaBonus() {
+        println("Bonus: 500")
     }
+
+}
+
+class Tecnico : Funcionario {
+    override fun calculaBonus() {
+        println("Bonus: 200")
+    }
+
+}
+
+class Analista : Funcionario {
+    override fun calculaBonus() {
+
+    }
+
 }
 
 fun main (){
 
-    val p = Pessoa()
-    p.geradorDeNome()
+    val f1: Funcionario = Gerente()
+    val f2: Funcionario = Tecnico()
+    val f3: Funcionario = Analista()
 
+    calculo(f1)
+    calculo(f2)
+
+}
+
+
+fun calculo(funcionario: Funcionario) {
+    funcionario.calculaBonus()
 }
